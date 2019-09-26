004461c2b27c776277ce1ef533c643fa1a6013c4
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-09-26 18:01:26 +0530

    Delete package-info.java

--- soap-over-https-client-certificate-authentication/spring-boot-soap-https-authentication-client/src/main/service/com/example/temperatureservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.com/TemperatureService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.example.temperatureservice;
